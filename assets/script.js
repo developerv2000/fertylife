9a82f3dc62fd069a9c8d003520e31bd3245a1e7b
MrBobPRO <bobnuridinov@gmal.com>
2024-07-11 17:25:48 +0500

    ModelViewer fixing

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 document.querySelectorAll('.navbar__link').forEach(anchor => {
-    console.log(anchor);
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
