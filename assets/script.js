cf60162cfb764d10cacf155b79622e1b0f450bac
MrBobPRO <bobnuridinov@gmal.com>
2024-07-22 16:07:09 +0500

    Intro image updated & sticky bottom added

--- assets/script.js
+++ assets/script.js
@@ -7,5 +7,20 @@ document.querySelectorAll('.navbar__link').forEach(anchor => {
     });
 });
 
-
 document.querySelector(".scroll-top").addEventListener("click", function () { document.body.scrollIntoView({ block: "start", behavior: "smooth" }) });
+
+// Function to check if the user has scrolled to the bottom
+function checkScroll() {
+    const sticky = document.querySelector('.sticky-bottom');
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    if (scrollPosition >= documentHeight) {
+        sticky.classList.add('sticky-bottom--fixed');
+    } else {
+        sticky.classList.remove('sticky-bottom--fixed');
+    }
+}
+
+// Add an event listener to the window scroll event
+window.addEventListener('scroll', checkScroll);
